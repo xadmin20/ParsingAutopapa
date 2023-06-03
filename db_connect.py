120e0abaeb9ec91dfb9e0b1e253a4102d91b9bd0
Nick <xadmin@bk.ru>
2023-06-03 15:40:37 +0400

    1.0 = Релиз

--- db_connect.py
+++ db_connect.py
@@ -4,63 +4,96 @@ from typing import Union
 import config
 
 db_client = pymongo.MongoClient(config.site_db)
+current_db = db_client["autopapa"]
+collection = current_db["autopapa"]
 
 
 def create_data_db(data_list: Union[dict]) -> bool:
-    """Создает БД"""
+    """Создает БД
+    :param data_list: dict словарь с данными об автомобиле
+    :return: Bool
+    """
     try:
-        current_db = db_client["autopapa"]
-        collection = current_db["autopapa"]
-        collection.insert_many(data_list)
-
-        for channel in collection.find():
-            print(channel.keys())
+        collection.insert_one(data_list)
+        config.logging.info(f"Добавлен документ {data_list['id']}")
         return True
     except Exception as e:
-        print("Error creating", e)
+        config.logging.error(f"Ошибка добавления документа {data_list['id']}", e)
         return False
 
 
 def car_link() -> list:
-    """Возвращает список ссылок на автомобили"""
+    """Возвращает список ссылок на автомобили
+    :return: list список ссылок на автомобили
+    """
     link: list = []
     try:
-        current_db = db_client["autopapa"]
-        collection = current_db["autopapa"]
         for channel in collection.find():
             car = channel.get("link")
             link.append(car)
+            config.logging.info(f"Получена ссылка на автомобиль {car}")
     except Exception as e:
-        print("Error reed", e)
+        config.logging.error(f"Ошибка получения ссылки на автомобиль", e)
         return []
     return link
 
 
 def test_double_delete() -> None:
-    """Удаляет дубликаты из БД"""
-    current_db = db_client["autopapa"]
-    collection = current_db["autopapa"]
+    """Удаляет дубликаты из БД
+    :return: None
+    """
     docs = list(collection.find())
     unique_docs: list = []
-
     for doc in docs:
         if doc['id'] not in unique_docs:
-            print(f"Добавлен уникальный документ {doc['id']}")
             unique_docs.append(doc['id'])
+            config.logging.info(f"Добавлен документ {doc['id']}")
         else:
             collection.delete_one({'id': doc['id']})
-            print(f"Удален дубликат {doc['id']}")
-    print(f"Удалено {len(docs) - len(unique_docs)} дубликатов")
+            config.logging.info(f"Удален документ {doc['id']}")
+    config.logging.info(f"Удалено {len(docs) - len(unique_docs)} дубликатов")
+
+
+def add_if_not_exists(auto: dict):
+    """Добавляет документ в БД, если его там нет, а если есть, то обновляет его
+    :param auto: dict словарь с данными об автомобиле
+    :return: Bool
+    """
+    config.logging.info(f"Добавление документа {auto['id']}")
+    if not collection.find_one({'id': auto['id']}):
+        result = collection.insert_one(auto)
+        config.logging.info(f"Добавлен документ {result.inserted_id}")
+        return True
+    else:
+        result = collection.update_one({'id': auto['id']}, {'$set': auto})
+        config.logging.info(f"Обновлен документ {result.modified_count}")
+        return False
 
 
 def test_openai():
-    """Тестовая функция"""
+    """Тестовая функция
+    :return: None
+    """
     test_double_delete()
-    current_db = db_client["autopapa"]
-    collection = current_db["autopapa"]
     docs = collection.find({'id': '772984'})
+    for doc in docs:
+        print(doc)
+        config.logging.info(f"Получен документ {doc['id']}")
+
+
+def test_print():
+    """Тестовая функция
+    :return: None
+    """
+    docs = list(collection.find({}))
     for doc in docs:
         print(doc)
 
 
-test_openai()
+def test_remove_documents_with_name_car():
+    """Удаляет документы с полем name_car
+    :return: None
+    """
+    result = collection.delete_many({'name_car': {'$exists': True}})
+    print(f"Deleted {result.deleted_count} documents")
+
--- parsing.py
+++ parsing.py
@@ -1,131 +1,170 @@
+import json
 import os
 import time
+import re
 
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import requests
 
+import config
 import db_connect
 
-url_main = 'https://autopapa.ge/ru/search?utf8=%E2%9C%93&s%5Bprice_from%5D=20000&s%5Bprice_to%5D=50000&s%5Bbrand_id' \
-           '%5D=0&s%5Bmodel_id%5D=0&s%5Bperiod%5D=0&s%5Byear_from%5D=0&s%5Byear_to%5D=0&s%5Bmileage_from%5D=0&s' \
-           '%5Bmileage_to%5D=0&s%5Bcountry_id%5D=2&s%5Bcity_id%5D=2&s%5Bcondition%5D=0&s%5Bengine_type%5D%5B%5D=14&s' \
-           '%5Bengine_capacity_from%5D=0&s%5Bengine_capacity_to%5D=0&s%5Bpower_from%5D=0&s%5Bpower_to%5D=0&s' \
-           '%5Bgearbox%5D%5B%5D=62&s%5Bdoor_qty%5D=0&s%5Bseats_count%5D=0&s%5Bmaterial%5D=0&s%5Bcolor_finish%5D=0'
 
-
-def html_code(url: str) -> BeautifulSoup:
+def html_code(url: str):
+    """Получение html кода страницы
+    :param url: https url страницы
+    :return dict {'url': url, 'BeautifulSoup': BeautifulSoup}
+    """
+    url_and_html = {}
     res = requests.get(url, headers={'User-Agent': UserAgent().random})
     soup: BeautifulSoup = BeautifulSoup(res.text, 'lxml')
-    return soup
+    url_and_html['url'] = url
+    url_and_html['html'] = soup
+    config.logging.info(f"Получен html код страницы {url}")
+    return url_and_html
 
 
-def parsing_auto(html: BeautifulSoup) -> list:
-    """Парсинг страницы с автомобилями"""
-    cars = html.find_all('div', class_='boxCatalog2')
+def parsing_auto(html: dict) -> list:
+    """Парсинг страницы с автомобилями
+    :param html: dict {'url': url, 'BeautifulSoup': BeautifulSoup}
+    :return: list [{'name_car': str, 'link': str, 'image_link': str, 'description': str}]
+    """
+    cars = html['html'].find_all('div', class_='boxCatalog2')
     # Собираем информацию для каждого автомобиля
     car_info_list: list = []
     for car in cars:
         info: dict = {}
         # Имя и тип автомобиля
         name_car = car.find('div', class_='titleCatalog')
-
         if name_car:
             info['name_car'] = name_car.text.strip()
         # Ссылка на автомобиль
         link = car.find('a', href=True)
-
         if link:
             info['link'] = f"https://autopapa.ge{link['href']}"
             info['id'] = str(link['href']).split('/')[-1]
         # Ссылка на изображение автомобиля
         image = car.find('img', src=True)
-
         if image:
             info['image_link'] = image['src']
-         # Описание автомобиля
+        # Описание автомобиля
         description = car.find('div', class_='paramCatalog')
-
         if description:
             cleaned_description = description.text.replace('\n', ' ').replace('\t', '').replace('\xa0', ' ')
             info['description'] = cleaned_description.strip()
         car_info_list.append(info)
 
     sorted_car_list = sorted(car_info_list, key=lambda x: int(x['id']), reverse=True)
+    config.logging.info(f"Получена информация о {len(sorted_car_list)} автомобилях")
     return sorted_car_list
 
 
-def get_info_car(html):
+def get_info_car(html) -> dict:
+    """Получение всей информации об автомобиле
+    :param html: dict {'url': url, 'BeautifulSoup': BeautifulSoup}
+    :return: dict {'name': str, 'price': str, 'link': str, 'currency': str, 'exchange_rate': str...}
     """
-    Получение всей информации об автомобиле
-    """
-    title_object = html.find('div', class_='titleObject')
-    name = title_object.contents[0].strip()
-    price = title_object.find('span', class_='priceObject').contents[0].strip()
-    currency = title_object.find('span', class_='lari').contents[0]
-    exchange_rate = title_object.find('a', class_='popup-calc').contents[0]
-    div = html.find('div', class_="contactObjectNew").find('div')
-    text_parts = div.contents[:4]
-    address = text_parts[0].strip()
-    customs_status = text_parts[3].get_text().replace(u'\xa0', u' ')
-    nameInfoObject = html.find_all('div', class_="nameInfoObject")
-
-    print(f"Название: {name}")
-    if html.find('div', class_='vincode-wrap-ads'):
-        print('VIN предоставлен')
-    else:
-        print('VIN НЕ предоставлен')
-    print(f"Цена: {price} {currency} ({exchange_rate})")
-    print(f"Адрес: {address} ({customs_status})")
-    for info_car in nameInfoObject:
-        print(info_car.text)
-
-
-def save_photo_car(url_list: list[str]):
-    """ Сохраняем все фотографии автомобиля в папку img """
+    info: dict = {}
+    soup = html
     try:
-        for url in url_list:
-            soup = html_code(url)
-            base_url: str = 'https://autopapa.ge'
-            for link in soup.find_all('a', class_='hidden-galler-images fancybox'):
-                img_url = base_url + link.get('href')
-                print(img_url)
-                img_id = url.split('/')[-1]
-                print(img_id)
-                dir_path = os.path.join('img', img_id)
-                os.makedirs(dir_path, exist_ok=True)
-                response = requests.get(img_url)
-                print(response)
-                filename = img_url.split('/')[-1].replace('?', '-')
-                orig_filename, id_filename = filename.split('-')
-                new_filename = f"{id_filename}-{orig_filename}"
-                print(new_filename)
-                file = open(os.path.join(dir_path, new_filename), "wb")
+        title_object = soup.find('div', class_='titleObject')
+        if title_object is not None:
+            name = title_object.contents[0].strip()
+        else:
+            name = ''
+        result = soup.find('title').text
+        title = re.search(r'\(# (\d+)\)', result)
+        if title is not None:
+            info['id'] = title.group(1)
+        price = title_object.find('span', class_='priceObject').contents[0].strip()
+        currency = title_object.find('span', class_='lari').contents[0]
+        exchange_rate = title_object.find('a', class_='popup-calc').contents[0]
+        div = html.find('div', class_="contactObjectNew").find('div')
+        text_parts = div.contents[:4]
+        address = text_parts[0].strip()
+        customs_status = text_parts[3].get_text().replace(u'\xa0', u' ')
+        nameInfoObject = html.find_all('div', class_="nameInfoObject")
+        print(f"Название: {name}")
+        if html.find('div', class_='vincode-wrap-ads'):
+            print('VIN предоставлен')
+        else:
+            print('VIN НЕ предоставлен')
+        print(f"Цена: {price} {currency} ({exchange_rate})")
+        print(f"Адрес: {address} ({customs_status})")
+        for info_car in nameInfoObject:
+            print(info_car.text)
+        time.sleep(2)
+        info['name'] = name
+        info['price'] = price
+        info['link'] = f"https://autopapa.ge/ru/text_search?text_s={info['id']}"
+        info['currency'] = currency
+        info['exchange_rate'] = exchange_rate
+        info['address'] = address
+        info['customs_status'] = customs_status
+        info['nameInfoObject'] = ' '.join([info_car.text for info_car in nameInfoObject])
+        send_to_db = db_connect.add_if_not_exists(info)
+        if send_to_db:
+            config.logging.info(f"Данные успешно отправлены в БД")
+        else:
+            config.logging.info(f"Данные не отправлены в БД")
+        return info
+    except Exception as e:
+        config.logging.error(f"Ошибка при получении информации об автомобиле {e}")
+
+
+def save_photo_car(url: str):
+    """ Сохраняем все фотографии автомобиля в папку img
+    :param url: str ссылка на страницу с автомобилем
+    :return: Bool
+    """
+    try:
+        config.logging.info(f"Сохраняем фотографии автомобилей")
+        soup = html_code(url=url)
+        base_url: str = 'https://autopapa.ge'
+        for link in soup['html'].find_all('a', class_='hidden-galler-images fancybox'):
+            img_url = base_url + link.get('href')
+            img_id = url.split('/')[-1]
+            dir_path = os.path.join('img', img_id)
+            os.makedirs(dir_path, exist_ok=True)
+            response = requests.get(img_url)
+            filename = img_url.split('/')[-1].replace('?', '-')
+            orig_filename, id_filename = filename.split('-')
+            new_filename = f"{id_filename}-{orig_filename}"
+            with open(os.path.join(dir_path, new_filename), "wb") as file:
                 file.write(response.content)
-                file.close()
-                time.sleep(1)
-            print(f"Автомобиль по адресу {url} сохранен")
+        config.logging.info(f"Автомобиль по адресу {url} сохранен")
         return True
     except Exception as e:
-        print(e)
+        config.logging.error(f"Ошибка при сохранении фотографий автомобиля {e}")
         return False
 
 
 def main():
-    """Главная функция"""
-    # result = create_data_db(parsing_auto(url=url))
-    # if result:
-    #     print("Данные успешно получены")
-    # else:
-    #     print("Ошибка!")
+    """ Главная функция
+    :return: None
+    """
+    result = parsing_auto(html=html_code(url=config.url_main))  # Список ссылок автомобилей
+    if result:
+        config.logging.info(f"Получена информация о {len(result)} автомобилях")
+    else:
+        config.logging.info(f"Не удалось получить информацию об автомобилях")
     #
-    for result in db_connect.car_link():
-        print(f'{result}')
-        get_info_car(html_code(url=result))
-        print('################################################')
+    for url_ in result:
+        html = html_code(url=url_['link'])
+        info_car = get_info_car(html=html['html'])
         time.sleep(3)
+        img_id = url_['link'].split('/')[-1]
+        dir_path = os.path.join('img', img_id)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(os.path.join(dir_path, f"{img_id}.json"), "w", encoding="utf-8") as file:
+            file.write(json.dumps(info_car, ensure_ascii=False, indent=4))
+        save_photo_car(url=url_['link'])
 
 
 if __name__ == '__main__':
-    """Запуск скрипта"""
+    """Запуск скрипта
+    :return: None
+    """
+    config.logging.info(f"Запуск скрипта")
     main()
